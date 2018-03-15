7f037129f92b30ff1dc6a3bf4a6bdd2a0a107379
Antonio Bozzano Schwedhelm <a.bozzanoschwedhel@c014.cip.loc>
2018-03-15 02:03:41 +0100

    commit before moving to VM

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate rusqlite;
+
+// use rusqlite;
+
 fn main() {
+    rusqlite::Connection::open("database.db");
     println!("Hello, world!");
 }
+
+
+// Report can be a String of ABCs (or YON), standing for Yay, Open, Nope
