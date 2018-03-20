ab0ebfb45ad629fa1dea9b37c3216d3386c1bad8
tonimctoni <nicoflam@outlook.com>
2018-03-20 17:19:29 +0100

    added get_poll to backend

--- README.md
+++ README.md
@@ -13,32 +13,32 @@ Web application for finding common interests in sport.
 
 # Frontend Design
 
- - Navbar has 4 options:
-    - Greeting
-        - Does nothing, just looks nice.
-    - Start poll
-        - Button for poll with predefined questions. Button for poll with free entries.
-        - Field for name: Only those who know the name may join. (Must be unique. Add button to check if already exists).
-        - Field for number of responders needed for result to be seen.
-        - Field for title: Hopefully descriptive title for poll.
-        - If predefined questions: Text entry for questions: Newline separated entries.
-        - If predefined questions: Some predefined questionnaires: Click to auto fill entry for questions (and maybe title).
-    - Fill poll
-        - Initial state:
-            - Field for poll name.
-            - Ok button.
-        - Poll name was selected state (and the name exists).
-            - Show poll title
-            - Input field for responder's name.
-            - If predefined questions: Questions, with buttons to select answer.
-            - Else: Text entry for responses: Newline separated entries.
-            - Button to submit responses.
-    - See poll results
-        - Initial state:
-            - Field for poll name.
-            - Ok button.
-        - Poll name was selected state (and the name exists).
-            - Results are shown if enough responders have responded.
+    - Navbar has 4 options:
+        - Greeting
+            - Does nothing, just looks nice.
+        - Start poll
+            - Button for poll with predefined questions. Button for poll with free entries.
+            - Field for name: Only those who know the name may join. (Must be unique. Add button to check if already exists).
+            - Field for number of responders needed for result to be seen.
+            - Field for title: Hopefully descriptive title for poll.
+            - If predefined questions: Text entry for questions: Newline separated entries.
+            - If predefined questions: Some predefined questionnaires: Click to auto fill entry for questions (and maybe title).
+        - Fill poll
+            - Initial state:
+                - Field for poll name.
+                - Ok button.
+            - Poll name was selected state (and the name exists).
+                - Show poll title
+                - Input field for responder's name.
+                - If predefined questions: Questions, with buttons to select answer.
+                - Else: Text entry for responses: Newline separated entries.
+                - Button to submit responses.
+        - See poll results
+            - Initial state:
+                - Field for poll name.
+                - Ok button.
+            - Poll name was selected state (and the name exists).
+                - Results are shown if enough responders have responded.
 
 # Backend Design
 
--- src/main.rs
+++ src/main.rs
@@ -59,7 +59,7 @@ struct ReceivedPoll {
     name: String,
     number: isize,
     title: String,
-    questions: Vec<String>,
+    questions: String,
 }
 
 #[post("/start_poll", format = "application/json", data = "<received_poll>")]
@@ -67,41 +67,91 @@ fn start_poll(polls: State<Mutex<HashMap<String, Poll>>>, received_poll: Json<Re
     let mut received_poll=received_poll.into_inner();
     received_poll.name.make_ascii_lowercase();
     received_poll.title.make_ascii_lowercase();
-    for q in received_poll.questions.iter_mut(){
-        (*q).make_ascii_lowercase();
-    }
+    let ReceivedPoll{name, number, title, questions}=received_poll;
+    let questions=questions
+    .lines()
+    .map(|l| {
+        let mut l=l.to_string();
+        l.make_ascii_lowercase();
+        l
+    })
+    .collect::<Vec<String>>();
 
-    if received_poll.name.len() < 3 || received_poll.name.len() > 32 || !contains_only(received_poll.name.as_str(), LOWER_ALPHANUMERIC_CHARS){
+
+    if name.len() < 3 || name.len() > 32 || !contains_only(name.as_str(), LOWER_ALPHANUMERIC_CHARS){
         return Json("Name length must be between 3 and 32 characters long and only contain alphanumeric characters.")
     }
 
-    if received_poll.number < 1 || received_poll.number > 1000{
+    if number < 1 || number > 1000{
         return Json("Number must be between 1 and 1000.")
     }
 
-    if received_poll.title.len() < 3 || received_poll.title.len() > 32 || !contains_only(received_poll.title.as_str(), LOWER_ALPHANUMERIC_SYMBOLS_CHARS){
+    if title.len() < 3 || title.len() > 32 || !contains_only(title.as_str(), LOWER_ALPHANUMERIC_SYMBOLS_CHARS){
         return Json("Title length must be between 3 and 32 characters long and only contain alphanumeric or these `!? ,;.:-_()[]{}&%$` characters.")
     }
 
-    if received_poll.questions.len()!=0 && (received_poll.questions.len() < 3 || received_poll.questions.len() > 1000){
+    if questions.len()!=0 && (questions.len() < 3 || questions.len() > 1000){
         return Json("There must be between 3 and 1000 questions.")
     }
 
-    if received_poll.questions.iter().any(|q| (*q).len() < 3 || (*q).len() > 32 || !contains_only((*q).as_str(), LOWER_ALPHA_SPACE_CHARS)){
+    if questions.iter().any(|q| (*q).len() < 3 || (*q).len() > 32 || !contains_only((*q).as_str(), LOWER_ALPHA_SPACE_CHARS)){
         return Json("The length of each question must be between 3 and 32, and only contain letters and spaces.")
     }
 
     match polls.lock() {
         Err(_) => Json("Server error."),
         Ok(mut polls) => {
-            if polls.contains_key(&received_poll.name){
+            if polls.contains_key(&name){
                 Json("A poll with that name already exists.")
             } else {
-                let ReceivedPoll{name, number, title, questions}=received_poll;
+                
                 let poll=Poll{number: number, title: title, questions: questions, answers: vec![]};
                 polls.insert(name, poll);
                 Json("success")
             }
+        },
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct UserAndPollName {
+    user_name: String,
+    poll_name: String,
+}
+
+#[post("/has_user_answered_poll", format = "application/json", data = "<user_and_poll_name>")]
+fn has_user_answered_poll(polls: State<Mutex<HashMap<String, Poll>>>, user_and_poll_name: Json<UserAndPollName>) -> Json<bool>{
+    let mut user_and_poll_name=user_and_poll_name.into_inner();
+    user_and_poll_name.user_name.make_ascii_lowercase();
+    user_and_poll_name.poll_name.make_ascii_lowercase();
+
+    match polls.lock() {
+        Err(_) => Json(true),
+        Ok(polls) => {
+            match polls.get(&user_and_poll_name.poll_name) {
+                None => Json(true),
+                Some(poll) => Json(poll.answers.iter().any(|a| (*a).0==user_and_poll_name.user_name)),
+            }
+        },
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct PollTitleAndQuestions {
+    title: String,
+    questions: Vec<String>,
+}
+
+#[post("/get_poll", format = "application/json", data = "<name>")]
+fn get_poll(polls: State<Mutex<HashMap<String, Poll>>>, name: Json<String>) -> Json<Option<PollTitleAndQuestions>>{
+    let mut name=name.into_inner();
+    name.make_ascii_lowercase();
+
+    match polls.lock() {
+        Err(_) => Json(None),
+        Ok(polls) => match polls.get(&name) {
+            None => Json(None),
+            Some(poll) => Json(Some(PollTitleAndQuestions{title: poll.title.clone(), questions: poll.questions.clone()})),
         },
     }
 }
