c3db701f68a0159f782dbb1577e97d95b3a4a559
Antonio Bozzano Schwedhelm <a.bozzanoschwedhel@c095.cip.loc>
2019-04-14 07:21:54 +0200

    backup

--- src/test.rs
+++ src/test.rs
@@ -33,13 +33,68 @@ fn put(client: &Client, input: StartPollInput) -> (Status, StartPollOutput){
     (status, output)
 }
 
+#[derive(Deserialize)]
+struct PollQuestions {
+    title: String,
+    questions: Vec<String>,
+    polls_filled: usize,
+    polls_number: isize,
+}
+
+#[derive(Deserialize)]
+struct PollAnswers {
+    title: String,
+    user_names: Vec<String>,
+    all_yay: Vec<String>,
+    all_open: Vec<String>,
+}
+
+#[derive(Deserialize)]
+pub struct GetPollOutput{
+    questions: Option<PollQuestions>,
+    answers: Option<PollAnswers>,
+    error: String,
+}
+
+fn get<T: AsRef<str>>(client: &Client, poll_id: T) -> (Status, GetPollOutput){
+    let mut response=client.get(format!("/api/poll/{}", poll_id.as_ref()))
+    .dispatch();
+
+    let status=response.status();
+    let output=if Status::Ok==status{
+        assert_eq!(response.content_type().expect("Expected content type"), ContentType::JSON);
+        let body=response.body_string().expect("Expected body string");
+        serde_json::from_str(body.as_str()).expect("Could not parse struct from body string")
+    } else{
+        GetPollOutput{questions: None, answers: None, error: String::new()}
+    };
+
+    (status, output)
+}
+
+#[derive(Serialize)]
+pub struct FillPollInput {
+    user_name: String,
+    fixed_answers: Option<Vec<isize>>,
+    free_answers: Option<String>,
+}
+
+fn post<T: AsRef<str>>(client: &Client, poll_id: T, poll: FillPollInput) -> (Status, String){
+    let mut response=client.post(format!("/api/poll/{}", poll_id.as_ref()))
+    .header(ContentType::JSON)
+    .body(json!(input).to_string())
+    .dispatch();
+
+    ...
+}
+
 #[test]
-fn api() {
+fn api_put() {
     let data=data::Data::new();
 
     let rocket=rocket::ignite()
     .manage(Mutex::new(data))
-    .mount("/api/", routes![func::put_poll, func::get_poll, func::post_poll]);
+    .mount("/api/", routes![func::put_poll]);
 
     let client = Client::new(rocket).expect("valid rocket instance");
 
@@ -63,6 +118,7 @@ fn api() {
         (2, "title", "question1"),
         (2, "title", "question1\n"),
         (2, "title", "question1\nquestion2\nquestion2\n"),
+        (2, "title", "question1\nquestion2\nqu\n"),
         (2, "title", "question1\nquestion2\nquestion<3>\n"),
     ].into_iter()
     .map(|(n,t,q)| (n, String::from(*t), String::from(*q)))
@@ -83,4 +139,54 @@ fn api() {
         assert!(poll_id=="");
         assert!(error!="");
     }
+}
+
+
+#[test]
+fn api_get_post() {
+    let data=data::Data::new();
+
+    let rocket=rocket::ignite()
+    .manage(Mutex::new(data))
+    .mount("/api/", routes![func::put_poll, func::get_poll, func::post_poll]);
+
+    let client = Client::new(rocket).expect("valid rocket instance");
+
+    // Get requests with invalid ids
+    let (status, output)=get(&client, "");
+    assert_ne!(status, Status::Ok);
+
+    let (status, output)=get(&client, "ABCDEF");
+    assert_eq!(status, Status::Ok);
+    assert!(output.questions.is_none());
+    assert!(output.answers.is_none());
+    assert!(output.error!="");
+
+    // Post requests with invalid ids
+
+    // FREE POLL
+    // start free poll
+    let (status, StartPollOutput{poll_id,..})=put(&client, StartPollInput{number: 2, title: String::from("title"), questions: String::new()});
+    assert_eq!(status, Status::Ok);
+
+    // get freshly started free poll
+    let (status, output)=get(&client, &poll_id);
+    assert_eq!(status, Status::Ok);
+    assert!(output.answers.is_none());
+    assert!(output.error!="");
+    let questions=output.questions.expect("Expected questions");
+    assert_eq!(questions.title, "title");
+    assert_eq!(questions.questions, Vec::new() as Vec<String>);
+    assert_eq!(questions.title, "title");
+    assert_eq!(questions.polls_filled, 0);
+    assert_eq!(questions.polls_number, 0);
+
+    // post invalid answers to free poll
+    // post first answer to free poll
+    // get free poll with one answer
+    // post second answer to free poll
+    // get fully answered free poll
+
+
+    // FIXED POLL
 }
